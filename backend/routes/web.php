09048efe354a22bc498168ac54ba7f8ffa15464e
Tung-NK <tungnguyenkhac222@gmail.com>
2025-04-27 22:56:17 +0700

    Add dashboard interface, login, logout function

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/',[AdminController::class,"login"])->name("admin.login");
+Route::post('admin/auth',[AdminController::class,"auth"])->name("admin.auth");
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("admin")->middleware("admin")->group(function() {
+    Route::get('dashboard',[AdminController::class,"index"])->name("admin.index");
+    Route::post('logout',[AdminController::class,"logout"])->name("admin.logout");
 });
