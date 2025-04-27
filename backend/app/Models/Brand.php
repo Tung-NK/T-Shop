aa77e5b1df8266f1df8d98cb642d9212135255fa
Tung-NK <tungnguyenkhac222@gmail.com>
2025-04-27 17:18:33 +0700

    create database

---
+++ backend/app/Models/Brand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function getRouteKeyName() // ghi đè slug thành khóa chính thay vì id
+    {
+        return 'slug';
+    }
+}
--- backend/database/migrations/2025_04_27_093307_create_admins_table.php
+++ backend/database/migrations/2025_04_27_093307_create_admins_table.php
@@ -11,19 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address')->nullable();
-            $table->string('city')->nullable();
-            $table->string('country')->nullable();
-            $table->string('zip_code')->nullable();
-            $table->string('phone_number')->nullable();
-            $table->string('profile_image')->nullable();
-            $table->boolean('profile_completed')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -34,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 };
